c9ee32b1e23b7e56b06b3353d8196793592014e0
DarthPjotr <DarthPjotr@users.noreply.github.com>
2021-07-08 15:16:49 +0200

    minikube config

---
+++ get_certs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+  cp /mnt/d/minikube/.minikube/profiles/minikube/client.crt ~/.kube/minikube/
+  cp /mnt/d/minikube/.minikube/profiles/minikube/client.key ~/.kube/minikube/
+  cp /mnt/d/minikube/.minikube/ca.crt ~/.kube/minikube/
