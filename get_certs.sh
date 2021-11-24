5b5b617ab91d4bc14133eca77a0bf0e5f3cbe7b8
DarthPjotr <DarthPjotr@users.noreply.github.com>
2021-11-24 15:01:18 +0100

    .

--- get_certs.sh
+++ get_certs.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-  cp /mnt/d/minikube/.minikube/profiles/minikube/client.crt ~/.kube/minikube/
-  cp /mnt/d/minikube/.minikube/profiles/minikube/client.key ~/.kube/minikube/
-  cp /mnt/d/minikube/.minikube/ca.crt ~/.kube/minikube/
+# C:\Users\pkvan\.minikube
+set -x
+
+KUBE_HOME=${HOME}/.kube
+WIN_HOME=/mnt/c/Users/pkvan
+MINIKUBE=${WIN_HOME}/.minikube
+
+mkdir -p ${KUBE_HOME}/minikube
+cp ${WIN_HOME}/.kube/config ${KUBE_HOME}/minikube.config
+# C:\Users\pkvan\.minikube\profiles\minikube\
+sed -i 's@C:\\Users\\pkvan\\.minikube\\profiles\\minikube\\@'${KUBE_HOME}'/minikube/@g' ${KUBE_HOME}/minikube.config
+# C:\Users\pkvan\.minikube\
+sed -i 's@C:\\Users\\pkvan\\.minikube\\@'${KUBE_HOME}'/minikube/@g' ${KUBE_HOME}/minikube.config
+
+cp ${MINIKUBE}/profiles/minikube/client.crt ${KUBE_HOME}/minikube/
+cp ${MINIKUBE}/profiles/minikube/client.key ${KUBE_HOME}/minikube/
+cp ${MINIKUBE}/ca.crt ${KUBE_HOME}/minikube/
